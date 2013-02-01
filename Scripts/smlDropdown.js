1b321f013348cde2a41ea8beba3afdfef26b32d6
gracian.ani@gmail.com <gracian.ani@gmail.com>
2013-01-31 21:21:40 -0600

    a

---
+++ Scripts/smlDropdown.js
@@ -0,0 +1,44 @@
+﻿(function ($, window) {
+
+  $.fn.smlDropdown = function (options) {
+	var i = i || 0;
+    // Default settings
+    var settings = $.extend({
+      "auto": true,             // Boolean: Animate automatically, true or false
+      "namespace": "smldropdown"  // String: change the default namespace used
+    }, options);
+
+    return this.each(function () {
+      // Index for namespacing
+      i++;
+      var $this = $(this),
+      // Namespacing
+        namespace = settings.namespace,
+        namespaceIdx = namespace + i,
+        $menu,
+        hideOtherMenu = function(){
+	        $('.smlDropdown.open').not($this).removeClass('open').find('.smlDropdown-menu').hide();
+        };
+        
+        $menu = $this.find('.smlDropdown-menu');
+        $menu.bind('click',function(e){
+        	if ( $menu[0] == e.currentTarget ) {
+	        	e.stopPropagation();
+        	}	
+        });
+    	$this.bind('click',function(e){
+    		e.stopPropagation();
+    		hideOtherMenu();
+	    	$this.toggleClass('open');
+	    	$menu.toggle();	    		
+    	});
+
+    	$(window).bind('click',function(e){
+    		
+	    		$this.removeClass('open');
+	    		$menu.hide();
+    		
+    	});
+    });
+ };
+})(jQuery,this);
